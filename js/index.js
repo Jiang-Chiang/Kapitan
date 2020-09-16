22c603739d94bb88b9e2efe2218154125dd1347c
Jiang-Chiang <republicmarch@gmail.com>
2020-09-16 18:00:35 +0800

    20091603更新

---
+++ js/index.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    $('#large_banner_block').lightSlider({
+        adaptiveHeight:true,
+        item:1,
+        slideMargin:0,
+        loop:true
+    });
+});
+
+$(document).ready(function() {
+    $('#yachts_list').lightSlider({
+        item:3,
+        auto:true,
+        loop:true,
+        pauseOnHover:true,
+        slideMove:1,
+        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+        speed:1000,
+        controls:false,
+        pager:false,
+        responsive : [
+            {
+                breakpoint:800,
+                settings: {
+                    item:3,
+                    slideMove:1,
+                    slideMargin:6,
+                  }
+            },
+            {
+                breakpoint:480,
+                settings: {
+                    item:2,
+                    slideMove:1
+                  }
+            }
+        ]
+    });  
+  });
