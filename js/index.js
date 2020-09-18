a49f5b66ddbe132bc404585f6bb8fbd905cdb342
Jiang-Chiang <republicmarch@gmail.com>
2020-09-19 01:12:50 +0800

    200919901更新

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// 輪播大BN：置頂
 $(document).ready(function() {
     $('#large_banner_list').lightSlider({
         adaptiveHeight:true,
@@ -7,6 +8,34 @@ $(document).ready(function() {
     });
 });
 
+// 手機達到Hover效果
+$(function () {
+    $(".inner a").attr('ontouchstart', 'hover(this)');//hover效果
+    $(".inner a").attr('ontouchend', 'mouseout(this)');//秒除hover
+})
+function mouseout(obj) {
+ var className = "hover";
+ var _ecname = obj.className;
+ if (_ecname.length == 0) return;
+ if (_ecname == className) {
+  obj.className = "";return;
+ }
+ if (_ecname.match(new RegExp("(^|\\s)" + className + "(\\s|$)")))
+  obj.className = _ecname.replace((new RegExp("(^|\\s)" + className + "(\\s|$)")), " ");
+}
+function hover(obj) {
+ if (!obj) return;
+ var className = "hover"
+ var _ecname = obj.className;
+ if (_ecname.length == 0) {
+  obj.className = className;return;
+ }
+ if (_ecname == className || _ecname.match(new RegExp("(^|\\s)" + className + "(\\s|$)")))
+  return;
+ obj.className = _ecname + " " + className;
+}
+
+// 輪播中BN：船艇
 $(document).ready(function() {
     $('#yachts_list').lightSlider({
         item:3,
@@ -37,3 +66,4 @@ $(document).ready(function() {
         ]
     });  
   });
+  
