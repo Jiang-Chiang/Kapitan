f661c98ff97d91dda96cd401af5a2ecef6ffdbfe
Jiang-Chiang <republicmarch@gmail.com>
2020-10-02 12:01:45 +0800

    20100201更新

--- js/index.js
+++ js/index.js
@@ -1,32 +1,32 @@
 // 手機達到Hover效果
 $(function () {
-    $(".inner a").attr('ontouchstart', 'hover(this)');//hover效果
-    $(".inner a").attr('ontouchend', 'mouseout(this)');//秒除hover
+  $(".inner a").attr('ontouchstart', 'hover(this)');//hover效果
+  $(".inner a").attr('ontouchend', 'mouseout(this)');//秒除hover
 })
 function mouseout(obj) {
- var className = "hover";
- var _ecname = obj.className;
- if (_ecname.length == 0) return;
- if (_ecname == className) {
-  obj.className = "";return;
- }
- if (_ecname.match(new RegExp("(^|\\s)" + className + "(\\s|$)")))
-  obj.className = _ecname.replace((new RegExp("(^|\\s)" + className + "(\\s|$)")), " ");
+  var className = "hover";
+  var _ecname = obj.className;
+  if (_ecname.length == 0) return;
+  if (_ecname == className) {
+    obj.className = ""; return;
+  }
+  if (_ecname.match(new RegExp("(^|\\s)" + className + "(\\s|$)")))
+    obj.className = _ecname.replace((new RegExp("(^|\\s)" + className + "(\\s|$)")), " ");
 }
 function hover(obj) {
- if (!obj) return;
- var className = "hover"
- var _ecname = obj.className;
- if (_ecname.length == 0) {
-  obj.className = className;return;
- }
- if (_ecname == className || _ecname.match(new RegExp("(^|\\s)" + className + "(\\s|$)")))
-  return;
- obj.className = _ecname + " " + className;
+  if (!obj) return;
+  var className = "hover"
+  var _ecname = obj.className;
+  if (_ecname.length == 0) {
+    obj.className = className; return;
+  }
+  if (_ecname == className || _ecname.match(new RegExp("(^|\\s)" + className + "(\\s|$)")))
+    return;
+  obj.className = _ecname + " " + className;
 };
 
 //scrollmagic動畫區間開始
-$(document).ready(function(){
+$(document).ready(function () {
   var controller = new ScrollMagic.Controller();
 
   var mv01 = TweenMax.fromTo('.mg', 1, {
@@ -39,35 +39,43 @@ $(document).ready(function(){
 
   var mv02 = TweenMax.fromTo('.mg1', 1, {
     x: -300,
-    opacity: 0
+    // opacity: 0,
+    display: 'none'
   }, {
     x: 0,
-    opacity: 1
+    // opacity: 1
+    display: 'block'
   });
 
   var mv03 = TweenMax.fromTo('.mg2', 1, {
     x: 300,
-    opacity: 0
+    // opacity: 0
+    display: 'none'
   }, {
     x: 0,
-    opacity: 1
+    // opacity: 1
+    display: 'block'
   });
 
   var mv04 = TweenMax.fromTo('.mg3', 1, {
     x: -300,
-    opacity: 0,
+    // opacity: 0,
+    display: 'none'
   }, {
     x: 0,
-    opacity: 1,
+    // opacity: 1,
+    display: 'block',
     delay: 0.4
   });
 
   var mv05 = TweenMax.fromTo('.mg4', 1, {
     x: 300,
-    opacity: 0
+    // opacity: 0
+    display: 'none'
   }, {
     x: 0,
-    opacity: 1,
+    // opacity: 1,
+    display: 'block',
     delay: 0.4
   });
 
@@ -105,5 +113,5 @@ $(document).ready(function(){
 
 
 
- 
+
 
