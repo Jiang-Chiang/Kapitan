502b57dea83846b9dd9e0000315269dd7e9f887b
Jiang-Chiang <republicmarch@gmail.com>
2020-10-04 20:50:18 +0800

    20100403更新

--- js/carousels.js
+++ js/carousels.js
@@ -1,108 +1,141 @@
 // 首頁置頂輪播大BN
-$(document).ready(function() {
+$(document).ready(function () {
     $('#large_banner_list').lightSlider({
-        item:1,
-        auto:true,
-        loop:true,
-        pauseOnHover:true,
-        slideMove:1,
+        item: 1,
+        auto: true,
+        loop: true,
+        pauseOnHover: true,
+        slideMove: 1,
         easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
-        speed:2000,
-        pause:5000,
-        adaptiveHeight:true,
-        responsive : [
+        speed: 2000,
+        pause: 5000,
+        adaptiveHeight: true,
+        responsive: [
             {
-                breakpoint:1199,
+                breakpoint: 1199,
                 settings: {
-                    item:1,
-                    slideMove:1,
-                    slideMargin:0,
-                    adaptiveHeight:true
-                  }
+                    item: 1,
+                    slideMove: 1,
+                    slideMargin: 0,
+                    adaptiveHeight: true
+                }
             },
             {
-                breakpoint:767,
+                breakpoint: 767,
                 settings: {
-                    item:1,
-                    slideMove:1
-                  }
+                    item: 1,
+                    slideMove: 1
+                }
             }
         ]
-    });  
-  });
+    });
+});
 
 //首頁船艇輪播中BN
-$(document).ready(function() {
+$(document).ready(function () {
     $('#yachts_list').lightSlider({
-        item:3,
-        auto:true,
-        loop:true,
-        pauseOnHover:true,
-        slideMove:1,
+        item: 3,
+        auto: true,
+        loop: true,
+        pauseOnHover: true,
+        slideMove: 1,
         easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
-        speed:2000,
-        pause:5000,
-        controls:false,
-        pager:false,
-        responsive : [
+        speed: 2000,
+        pause: 5000,
+        controls: false,
+        pager: false,
+        responsive: [
             {
-                breakpoint:1199,
+                breakpoint: 1199,
                 settings: {
-                    item:1,
-                    slideMove:1,
-                    slideMargin:0,
-                  }
+                    item: 1,
+                    slideMove: 1,
+                    slideMargin: 0,
+                }
             },
             {
-                breakpoint:767,
+                breakpoint: 767,
                 settings: {
-                    item:1,
-                    slideMove:1
-                  }
+                    item: 1,
+                    slideMove: 1
+                }
             }
         ]
-    });  
-  });
+    });
+});
 
 //首頁引言輪播小BN
-$(document).ready(function() {
+$(document).ready(function () {
     $('#prologue_img_list').lightSlider({
-        item:1,
-        auto:true,
-        loop:true,
-        pauseOnHover:true,
-        slideMove:1,
-        mode:'fade',
+        item: 1,
+        auto: true,
+        loop: true,
+        pauseOnHover: true,
+        slideMove: 1,
+        mode: 'fade',
         easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
-        speed:2000,
-        pause:5000,
-        controls:false,
-        responsive : [
+        speed: 2000,
+        pause: 5000,
+        controls: false,
+        responsive: [
             {
-                breakpoint:1199,
+                breakpoint: 1199,
                 settings: {
-                    item:1,
-                    slideMove:1,
-                    slideMargin:0,
-                  }
+                    item: 1,
+                    slideMove: 1,
+                    slideMargin: 0,
+                }
             },
             {
-                breakpoint:767,
+                breakpoint: 767,
                 settings: {
-                    item:1,
-                    slideMove:1
-                  }
+                    item: 1,
+                    slideMove: 1
+                }
             }
         ]
-    });  
-  });
-  
+    });
+});
+
 //分頁置頂輪播中BN
-$(document).ready(function() {
+$(document).ready(function () {
     $('#middle_banner_block').lightSlider({
-        adaptiveHeight:true,
-        item:1,
-        slideMargin:0,
-        loop:true
+        adaptiveHeight: true,
+        item: 1,
+        slideMargin: 0,
+        loop: true
+    });
+});
+
+//商品頁（船艇）大頭貼輪播小BN
+$(document).ready(function () {
+    $('#yacht_order_avatar_list').lightSlider({
+        item: 1,
+        auto: true,
+        loop: true,
+        pauseOnHover: true,
+        slideMove: 1,
+        mode: 'fade',
+        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+        speed: 2000,
+        pause: 5000,
+        controls: false,
+        responsive: [
+            {
+                breakpoint: 1199,
+                settings: {
+                    item: 1,
+                    slideMove: 1,
+                    slideMargin: 0,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    item: 1,
+                    slideMove: 1
+                }
+            }
+        ]
     });
 });
--- js/yachts_poseidon.js
+++ js/yachts_poseidon.js
@@ -17,3 +17,22 @@ $(function () {
         $content.eq($tabIndex).show().siblings().hide();
     });
 });
+
+$(function () {
+    var $tabPanel = $('#tab_panel'),
+        $tabs = $tabPanel.find('.tabs_for_mobile'),
+        $tab = $tabs.find('a'),
+        $tabContent = $tabPanel.find('.tabs_content'),
+        $content = $tabContent.find('> li');
+
+    $tab.eq(0).addClass('active');
+    $content.eq(0).show();
+
+    $tab.on('click', function () {
+        var $tabIndex = $(this).index();
+        $(this).addClass('active').siblings().removeClass('active');
+        $(this).find('div.tab_btn').addClass('_present').parent('a').siblings().find('div.tab_btn').removeClass('_present');
+        $(this).find('img.selected_icon').show().siblings('img.normal_icon').hide().parents('a').siblings().find('img.normal_icon').show().siblings('img.selected_icon').hide();
+        $content.eq($tabIndex).show().siblings().hide();
+    });
+});
